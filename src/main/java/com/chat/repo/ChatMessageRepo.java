c15207537df1efee87299baa371bc8632392ba69
swaran21 <saiswaran212005@gmail.com>
2025-03-30 23:44:44 +0530

    Mongo changes and delete option

---
+++ src/main/java/com/chat/repo/ChatMessageRepo.java
@@ -0,0 +1,10 @@
+package com.chat.repo;
+
+import com.chat.model.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface ChatMessageRepo extends MongoRepository<ChatMessage, String> {
+    List<ChatMessage> findByChatId(Long chatId);
+}
